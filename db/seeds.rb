a78bfd0d1cc001c80593efe4112797c0fc94337b
zhiyong tan <zhiyong@uber.com>
2016-05-22 18:11:08 -0700

    Added admin flag to User model, and seeded the first admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+unless User.exists?(email: "admin@ticketee.com")
+  User.create!(email: "admin@ticktee.com", password: "password", admin: true)
+end
+unless User.exists?(email: "viewer@ticketee.com")
+  User.create!(email: "viewer@ticketee.com", password: "password")
+end
+["Sublime Text 3", "Internet Explorer"].each do |name|
+  unless Project.exists?(name: name)
+    Project.create!(name: name, description: "A sample project about #{name}")
+  end
+end
