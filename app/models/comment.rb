48b786ff7e13fce615bec1a5a13db2885aaf33fb
zhiyong tan <zhiyong@uber.com>
2016-05-26 22:21:49 -0700

    Authorized users can now leave comments on tickets

---
+++ app/models/comment.rb
@@ -0,0 +1,7 @@
+class Comment < ActiveRecord::Base
+  belongs_to :ticket
+  belongs_to :author, class_name: "User"
+  scope :persisted, lambda { where.not(id: nil) }
+  validates :text, presence: true
+  delegate :project, to: :ticket
+end
