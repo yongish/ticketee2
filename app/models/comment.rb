d6a2e6772562439979b78bdc5957d285f5e7b4df
zhiyong tan <zhiyong@uber.com>
2016-05-27 23:04:36 -0700

    Display a comment's state transition

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ActiveRecord::Base
+  belongs_to :previous_state, class_name: "State"
   belongs_to :state
   belongs_to :ticket
   belongs_to :author, class_name: "User"
@@ -6,8 +7,14 @@ class Comment < ActiveRecord::Base
   validates :text, presence: true
   delegate :project, to: :ticket
   after_create :set_ticket_state
+  before_create :set_previous_state
+  after_create :set_ticket_state
 
   private
+
+  def set_previous_state
+    self.previous_state = ticket.state
+  end
 
   def set_ticket_state
     ticket.state = state
